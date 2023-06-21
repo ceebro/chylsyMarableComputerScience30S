eda162b7ab7eeccb6e38d0d0404458211a0104b8
chylsy <128323856+ceebro@users.noreply.github.com>
2023-06-21 14:17:22 -0500

    Update TextFR.java

--- chylsyMarableIslandAdventure/src/chylsymarableislandadventure/TextFR.java
+++ chylsyMarableIslandAdventure/src/chylsymarableislandadventure/TextFR.java
@@ -24,7 +24,7 @@ public class TextFR {
                 + "\n mystérieuse. Vous avez besoin de collecter des ressources."
                 + "\n Vous avez le choix d’explorer la plage ou la jungle. Lequel"
                 + "\n explorez-vous?";
-        //sit initiale
+        //initial situation
         
         textFR[1] = "   Vous marchez à côté de la plage et vous trouvez un avion"
                 + "\n détruit. Dedans, il y a des outils ainsi qu’un bateau de "
@@ -37,59 +37,52 @@ public class TextFR {
         textFR[3] = "   Vous choisissez de rester sur l’île, potentiellement pour"
                 + "\n toujours. Au lieu d’explorer la plage, vous allez dans la "
                 + "\n jungle pour l’explorer. ";
-        
+        //program jumps to jungle option
+
         textFR[4] = "   Vous dépensez vos jours en flottant sur la mer. Vous avez"
                 + "\n faim et fatigué. Éventuellement, vouz mourez dans l'océan.";
-               
+        //ocean ending
+        
         textFR[5] = "   Les pêcheurs vous donnent de la nourriture et de la protection."
                 + "\n Éventuellement, vous retournez à la société.";
-        //\n
+        //society ending
+        
         textFR[6] = "   Pendant que vous marchez dans la jungle, vous trouvez une"
                 + "\n grotte. Il est sombre et mouillé. Voulez-vous l'explorer?";
         
-        //' 
         textFR[7] = "   Il y a un ours dans la grotte! Il se sent fâché à votre arrivé. "
                 + "\n Vous pouvez le combattre ou s'en fuir. Lequel choisissez-vous?";
         
         textFR[8] = "   L'ours vous attrapez. Il vous attaque et vous mourez de vos blessures.";
-        
+        //mauling ending
+
         textFR[9] = "   Par miracle, l'ours s'en fuit de la grotte. Vous pouvez"
                 + "\n maintenant s'habiter dans la grotte ou continuer votre"
                 + "\n voyage.";
-        
-//        "      You make your own habitat out of the abandoned cave. You "
-//                + "\n live off the land, farming and foraging. You eventually "
-//                + "\n die on the island, warm and secure.";
-        
-        textFR[10] = "  Vous creez votre propre habitat dans la grotte. Vous"
-                + "\n continuez votre vie  ";
-        
-        // "      You walk away from the cave and decide to keep exploring"
-               // + "\n the island.";
-                
-        textFR[11] = "";
-        
-//        "      Exploring the jungle has led you to a settlement. Do you "
-//                + "\n enter this village, or do you walk away?";
-        textFR[12] = "";
-        
-//         "      You decide to enter the village. Initially, the inhabitants"
-//                + "\n are cautious of you, but soon come to accept your presence."
-//                + "\n You learn to speak their language and eventually fully "
-//                + "\n integrate into their society.";
-        textFR[13] = "";
-        
-//        "      You wander senselessly through the jungle for days. Your "
-//                + "\n body is exhausted, and you are starving. Eventually, you "
-//                + "\n die of malnourishment.";
-        textFR[14] = "";
-        
-        //        textEN[15] = "Thank you for playing Island Adventure!";
-        textFR[15] = "";
-        
-        //        textEN[16] = "Do you want to play again?";
+        //minecraft ending
 
-        textFR[16] = "";
+        textFR[10] = "  Vous creez votre propre habitat dans la grotte. Vous"
+                + "\n continuez le restant de votre vie  dans la grotte. Éventuellement,"
+                + "\n vous mourez sur l'île, heureux et en sécurité.";
+        
+        textFR[11] = "  Vous continuez votre voyage en explorant l'île plus fortement.";
+        //program jumps to village option
+
+        textFR[12] = "  En explorant la jungle, vous trouvez un village. Voulez-vous"
+                + "\n l'entrer ou continuer la voyage?";
+        
+        textFR[13] = "  Vous décidez d'entrer le village. Au début, les habitants"
+                + "\n du village s'inquietent de vous. Mais avec du temps, vous"
+                + "\n intégrez avec leur communauté comme un autre villageois.";
+        //minecraft village ending
+
+        textFR[14] = "  Votre voyage en marchant insensément arrête avec votre décès."
+                + "\n Vous mourez sur l'île avec un corps épuisé et affamé.";
+        //starvation ending
+
+        textFR[15] = "Merci pour avoir joué Island Adventure!";
+        
+        textFR[16] = "Voulez-vous jouer encore?";
 
         //</editor-fold>
         
